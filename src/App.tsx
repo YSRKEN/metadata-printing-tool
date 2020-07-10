233504bf667774e806118bd660291b4ffeab2a2f
YSRKEN <ysr.ken@gmail.com>
2020-07-10 22:26:35 +0900

    [add] Make=Panasonicにおけるレンズ名に対応した

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,35 @@ import { Container, Row, Col, Form } from 'react-bootstrap';
 import 'App.css';
 import EXIF from 'exif-js';
 
+const getLensNameForPanasonic = (makerNote: number[], rawData: ArrayBuffer) => {
+  const ifdCount = makerNote[12];
+  for (let i = 0; i < ifdCount; i += 1) {
+    const ifdIndex = 12 + 2 + 12 * i;
+    const temp = makerNote.slice(ifdIndex, ifdIndex + 12);
+    const tagId = temp[1] * 256 + temp[0];
+    if (tagId === 0x51) {
+      // データの長さ・オフセット値を取得
+      const lensNameLength = temp[7] * 16777216 + temp[6] * 65536 + temp[5] * 256 + temp[4];
+      const lensNameOffset = temp[11] * 16777216 + temp[10] * 65536 + temp[9] * 256 + temp[8];
+
+      // 検索処理で文字列をスライスして取り出す
+      const keyWord = 'Exif';
+      const view = new Uint8Array(rawData);
+      for (let j = 0; j < Math.min(view.length - 4, 3000); j += 1) {
+        if (view[j] === keyWord.charCodeAt(0)
+          && view[j + 1] === keyWord.charCodeAt(1)
+          && view[j + 2] === keyWord.charCodeAt(2)
+          && view[j + 3] === keyWord.charCodeAt(3)) {
+            const lensNameArray = view.slice(j + 6 + lensNameOffset, j + 6 + lensNameOffset + lensNameLength);
+            const lensName = (new TextDecoder('UTF-8')).decode(lensNameArray).replace(/\0/g, '');
+            return lensName;
+          }
+      }
+    }
+  }
+  return '？？';
+};
+
 const App = () => {
   const [maker, setMaker] = useState('？');
   const [model, setModel] = useState('？');
@@ -23,41 +52,50 @@ const App = () => {
     const files: FileList = e.dataTransfer.files;
     if (files.length >= 1) {
       const file = files[0];
-      EXIF.getData(file as any, () => {
-        const exif: {[key: string]: any} = EXIF.getAllTags(file);
-        if ('Make' in exif) {
-          setMaker((exif['Make'] as string).replace(/\0/g, ''));
+      const reader = new FileReader();
+      reader.onload = () => {
+        const rawData = reader.result;
+        if (rawData !== null && typeof rawData !== 'string') {
+          EXIF.getData(file as any, () => {
+            const exif: { [key: string]: any } = EXIF.getAllTags(file);
+            if ('Make' in exif) {
+              setMaker((exif['Make'] as string).replace(/\0/g, ''));
+            }
+            if ('Model' in exif) {
+              setModel((exif['Model'] as string).replace(/\0/g, ''));
+            }
+            if ('ExposureTime' in exif) {
+              const rawShutterSpeed: Number = exif['ExposureTime'];
+              if (rawShutterSpeed.valueOf() < 1.0) {
+                setShutterSpeed(`1/${Math.round(1.0 / rawShutterSpeed.valueOf())}`);
+              } else {
+                setShutterSpeed(rawShutterSpeed.toString());
+              }
+            }
+            if ('FNumber' in exif) {
+              const rawFNumber: Number = exif['FNumber'];
+              setFNumber(rawFNumber.toString());
+            }
+            if ('ISOSpeedRatings' in exif) {
+              setIsoRate(`ISO${exif['ISOSpeedRatings']}`);
+            }
+
+            // メーカー毎に分析処理が分岐
+            if ('Make' in exif) {
+              const rawMaker = (exif['Make'] as string);
+              if (rawMaker.includes('OLYMPUS') && 'undefined' in exif) {
+                setLensName((exif['undefined'] as string).replace(/\0/g, ''));
+              } else if (rawMaker.includes('Panasonic') && 'MakerNote' in exif) {
+                setLensName(getLensNameForPanasonic(exif['MakerNote'], rawData));
+              } else {
+                setLensName('？');
+                console.log(exif);
+              }
+            }
+          });
         }
-        if ('Model' in exif) {
-          setModel((exif['Model'] as string).replace(/\0/g, ''));
-        }
-        if ('ExposureTime' in exif) {
-          const rawShutterSpeed: Number = exif['ExposureTime'];
-          if (rawShutterSpeed.valueOf() < 1.0) {
-            setShutterSpeed(`1/${Math.round(1.0 / rawShutterSpeed.valueOf())}`);
-          } else {
-            setShutterSpeed(rawShutterSpeed.toString());
-          }
-        }
-        if ('FNumber' in exif) {
-          const rawFNumber: Number = exif['FNumber'];
-          setFNumber(rawFNumber.toString());
-        }
-        if ('ISOSpeedRatings' in exif) {
-          setIsoRate(`ISO${exif['ISOSpeedRatings']}`);
-        }
-        
-        // メーカー毎に分析処理が分岐
-        if ('Make' in exif) {
-          const rawMaker = (exif['Make'] as string);
-          if (rawMaker.includes('OLYMPUS')) {
-            setLensName((exif['undefined'] as string).replace(/\0/g, ''));
-          } else {
-            setLensName('？');
-            console.log(exif);
-          }
-        }
-      });
+      };
+      reader.readAsArrayBuffer(file);
     }
   };
 
