a6fb3bc2e232452483188ef7bccbc44957a469ba
YSRKEN <ysr.ken@gmail.com>
2020-07-12 05:02:18 +0900

    [add] プレビュー部分の高さを設定した

--- src/container/PhotoView.tsx
+++ src/container/PhotoView.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PhotoView: React.FC = () => {
   return (
-    <div className="border d-flex justify-content-center flex-column align-items-center">
+    <div className="border d-flex justify-content-center flex-column align-items-center h-100">
       <strong className="d-block">
         ここにドラッグ＆ドロップ
       </strong>
