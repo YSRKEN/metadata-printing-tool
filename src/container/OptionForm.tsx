08ebedacbf4c62d31e7b356041e17c1ee195dfff
YSRKEN <ysr.ken@gmail.com>
2020-07-12 05:06:23 +0900

    [add] 画面右側のフォーム部分について、JSXを修正してレイアウトを良くした。また、<input>ではなく<select>な箇所について修正した

--- src/container/OptionForm.tsx
+++ src/container/OptionForm.tsx
@@ -7,22 +7,42 @@ const OptionForm: React.FC = () => {
       <Form.Group>
         <Form.Label htmlFor="maker">メーカー</Form.Label>
         <Form.Control id="maker" />
+      </Form.Group>
+      <Form.Group>
         <Form.Label htmlFor="model">モデル名</Form.Label>
         <Form.Control id="model" />
+      </Form.Group>
+      <Form.Group>
         <Form.Label htmlFor="lens_name">レンズ名</Form.Label>
         <Form.Control id="lens_name" />
+      </Form.Group>
+      <Form.Group>
         <Form.Label htmlFor="exposure_time">露光時間</Form.Label>
         <Form.Control id="exposure_time" />
+      </Form.Group>
+      <Form.Group>
         <Form.Label htmlFor="f_number">F値</Form.Label>
         <Form.Control id="f_number" />
+      </Form.Group>
+      <Form.Group>
         <Form.Label htmlFor="iso_rate">ISO感度</Form.Label>
         <Form.Control id="iso_rate" />
       </Form.Group>
       <Form.Group>
         <Form.Label htmlFor="text_position">表示位置</Form.Label>
-        <Form.Control id="text_position" />
+        <Form.Control id="text_position" as="select">
+          <option value="lb">左下</option>
+          <option value="rb">右下</option>
+          <option value="rt">右上</option>
+          <option value="lt">左上</option>
+        </Form.Control>
+      </Form.Group>
+      <Form.Group>
         <Form.Label htmlFor="text_color">表示色</Form.Label>
-        <Form.Control id="text_color" />
+        <Form.Control id="text_color" as="select">
+          <option value="w">明るい色</option>
+          <option value="b">暗い色</option>
+        </Form.Control>
       </Form.Group>
     </Form>
   );
